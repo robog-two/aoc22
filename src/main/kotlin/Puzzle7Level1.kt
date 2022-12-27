cfec2f1fb40e091121aad9acf825d14709f9da36
Sam Knight <robogamingbysam@icloud.com>
2022-12-26 22:27:26 -0600

    Day 7

--- src/main/kotlin/Puzzle7Level1.kt
+++ src/main/kotlin/Puzzle7Level1.kt
@@ -4,44 +4,61 @@ import java.nio.file.Path
 
 object Puzzle7Level1 : PuzzleSolver {
     override fun solve(puzzle: String): String {
+        val directorySizes = getDirectorySizes(puzzle)
+
+        return directorySizes.values.sorted().reduce { acc, i -> if (i <= 100000) acc + i else acc }.toString()
+    }
+
+    fun getDirectorySizes(puzzle: String): MutableMap<String, Int> {
         val consoleLines = puzzle.trim().split("\n")
         var currentPath = Path.of("/")
         var currentSize = 0
-        val directorySizes = mutableListOf<Pair<Path, Int>>()
-        for (line in consoleLines) {
-            when {
-                line.startsWith("$ cd ") -> {
-                    if (currentSize > 0) {
-                        directorySizes.add(Pair(currentPath, currentSize))
+        val directorySizes = mutableMapOf<String, Int>()
+        var keepSearching = true
+        // This is the definition of insanity.
+        // TODO: refactor to not be insane.
+        for (i in 1..100000) {
+            keepSearching = false
+            currentPath = Path.of("/")
+            currentSize = 0
+            var maxRecurse = 0
+            for (line in consoleLines) {
+                when {
+                    line.startsWith("$ cd ") -> {
+                        if (currentSize > 0) {
+                            directorySizes[currentPath.toAbsolutePath().toString()] = currentSize
+                            currentSize = 0
+                        }
+                        var moveTo = line.substring(5, line.length)
+                        if (moveTo == "..") {
+                            currentPath = currentPath.parent
+                        } else {
+                            currentPath = Path.of(currentPath.toAbsolutePath().toString(), moveTo)
+                        }
+                    }
+
+                    line.startsWith("$ ls") -> {
                         currentSize = 0
                     }
-                    var moveTo = line.substring(5, line.length)
-                    if (moveTo == "..") {
-                        currentPath = currentPath.parent
-                    } else {
-                        currentPath = Path.of(currentPath.toAbsolutePath().toString(), moveTo)
-                    }
-                    println(currentPath)
-                }
-                line.startsWith("$ ls") -> {
-                    currentSize = 0
-                }
-                else -> {
-                    if (line.split(" ").first() != "dir") {
-                        currentSize += line.split(" ").first().toInt()
+
+                    else -> {
+                        if (line.split(" ").first() != "dir") {
+                            currentSize += line.split(" ").first().toInt()
+                        } else {
+                            currentSize += directorySizes.getOrElse(Path.of(currentPath.toAbsolutePath().toString(), line.split(" ").last()).toAbsolutePath().toString()) {
+                                keepSearching = true
+                                0
+                            }
+                        }
                     }
                 }
+            }
+            if (currentSize > 0) {
+                directorySizes[currentPath.toAbsolutePath().toString()] = currentSize
+                currentSize = 0
             }
         }
 
-        return directorySizes.map { it.second }.sortedByDescending { it }.reduce { acc, pair ->
-            if (pair < 100000) {
-                acc + pair
-            } else if (acc < 100000) {
-                acc
-            } else {
-                0
-            }
-        }.toString()
+        return directorySizes
     }
 }
---
+++ src/main/kotlin/Puzzle7Level2.kt
@@ -0,0 +1,20 @@
+import Puzzle5Level1.extractBoard
+import Puzzle6Level1.hasDupe
+import Puzzle7Level1.getDirectorySizes
+import java.nio.file.Path
+
+object Puzzle7Level2 : PuzzleSolver {
+    override fun solve(puzzle: String): String {
+        val directorySizes = getDirectorySizes(puzzle)
+
+        val spaceLeft = 70000000 - directorySizes["/"]!! // yum :P
+        val spaceNeeded = 30000000 - spaceLeft
+
+        println(directorySizes["/"]!!)
+        println(spaceNeeded)
+        println(spaceLeft)
+        println(directorySizes)
+
+        return directorySizes.map { Pair(it.key, it.value) }.sortedByDescending { it.second }.last { it.second >= spaceNeeded }.second.toString()
+    }
+}
